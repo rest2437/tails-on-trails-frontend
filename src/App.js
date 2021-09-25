e6c31a6bf33f9cd2405c9d0528d7fe877b8312e7
rest2437 <nocturnalrd@gmail.com>
2021-09-25 10:41:23 -0700

    Changes: added signup and login page, also set boilerplate for each page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter, Route, NavLink } from "react-router-dom";
 import Title from "./components/TitlePage";
+
 function App() {
   return (
     <div className="App">
